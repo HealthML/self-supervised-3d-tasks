cf28028b5e2b2e78f3ed485c85e835102a24170d
tommysft <thgaertner.sft@t-online.de>
2019-11-08 12:11:14 +0100

    Revert "cpc encoder in models"

--- models/encoder_cpc.py
+++ models/encoder_cpc.py
@@ -1,8 +1,6 @@
 """Implements Encder model for Contrastive prediction model.
 """
 
-import functools
-
 import tensorflow as tf
 
 
@@ -10,37 +8,49 @@ def batch_norm(x, training):
     return tf.layers.batch_normalization(x, fused=True, training=training)
 
 
-def encoder(x,  # pylint: disable=missing-docstring
-            is_training,
-            num_layers,
+def encoder(x,
+            is_training: bool,
+            num_layers: int = 3,
             strides=(2, 2, 2),
-            num_classes=1000,
-            filters_factor=4,
-            weight_decay=1e-4,
-            include_root_block=True,
-            root_conv_size=7, root_conv_stride=2,
-            root_pool_size=3, root_pool_stride=2,
+            outputs: int = 1000,
+            filters: int = 4,
+            weight_decay: float = 1e-4,
+            kernel_size: int = 7,
             activation_fn=tf.nn.relu,
-            last_relu=True,
             normalization_fn=batch_norm,
-            global_pool=True):
-    end_points = {}
+            ):
+    '''
+    Args:
+        x: Input layer
+        is_training (bool): if the model is training
+        num_layers (int): number of convolution layer
+        strides (tuple): strides for convolution layer
+        outputs (int): number of outputs
+        filters (int): number of filters in convolution layer
+        weight_decay (float): scale for kernel regularization
+        kernel_size: size of kernel in convolutions
+        activation_fn: activation function of dense layer
+        normalization_fn: normalization function after convolution layers
 
-    filters = 16 * filters_factor
+    Returns:
+        x: last dense layer
+    '''
 
     kernel_regularizer = tf.contrib.layers.l2_regularizer(scale=weight_decay)
 
-    x = tf.layers.conv2d(x, filters=filters,
-                         kernel_size=root_conv_size,
-                         strides=root_conv_stride,
-                         padding='VALID', use_bias=False,
-                         kernel_regularizer=kernel_regularizer)
+    for i in range(1, num_layers):
+        x = tf.layers.conv2d(x,
+                             filters=filters,
+                             kernel_size=kernel_size,
+                             strides=strides,
+                             padding='VALID', use_bias=False,
+                             kernel_regularizer=kernel_regularizer)
 
-    x = normalization_fn(x, training=is_training)
+        x = normalization_fn(x, training=is_training)
 
-    end_points['after_root'] = x
+    x = tf.layers.dense(x,
+                        units=outputs,
+                        activation=activation_fn,
+                        )
 
-    return end_points['pre_logits'], end_points
-
-
-cpc_encoder = functools.partial(encoder)
+    return x
