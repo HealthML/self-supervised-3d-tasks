a821f7e6161a37cf86e8f08fb550a4b8777f94f6
hv10 <noeldanz@gmail.com>
2019-11-21 17:02:36 +0100

    introduce new error on redefinition in already mapped functions update signature_inspection example to reflect more pythonic approach (as recomm. by pydocs) update flag_utils to check if a param has been mapped before (and then by default reject the remapping)

--- self_supervised_3d_tasks/errors.py
+++ self_supervised_3d_tasks/errors.py
@@ -4,6 +4,12 @@ class MissingFlagsError(Exception):
         self.missing_flags = missing_flags
 
     def __str__(self):
-        return (
-            f'For the method "{self.method}" the flag(s) {self.missing_flags} is required.'
-        )
+        return f'For the method "{self.method}" the flag(s) {self.missing_flags} is required.'
+
+
+class MappingRedefinitionError(Exception):
+    def __init__(self, variable):
+        self.variable = variable
+
+    def __str__(self):
+        return f"The attempt to redefine the variable {self.variable} is not allowed in this context."
