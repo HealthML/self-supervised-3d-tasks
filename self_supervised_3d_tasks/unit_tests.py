4e07cc978d56d7cfd853f5d8520802f2477a5099
Winfried Loetzsch <winfried.loetzsch@student.hpi.de>
2019-12-17 13:23:55 +0100

    retina dataset implemented. some tests for brain dataset

--- self_supervised_3d_tasks/unit_tests.py
+++ self_supervised_3d_tasks/unit_tests.py
@@ -1,18 +1,18 @@
 import functools
+from os.path import expanduser
 
 import absl.flags as flags
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-from .algorithms.patch_model_preprocess import get_crop_patches_fn
-from .datasets import get_data
-from .preprocess import get_crop, get_random_flip_ud, get_drop_all_channels_but_one_preprocess, \
+from self_supervised_3d_tasks.algorithms.patch_model_preprocess import get_crop_patches_fn
+from self_supervised_3d_tasks.datasets import get_data
+from self_supervised_3d_tasks.preprocess import get_crop, get_random_flip_ud, get_drop_all_channels_but_one_preprocess, \
     get_pad
 
 
 def plot_sequences(x, y, labels=None, output_path=None):
-
     ''' Draws a plot where sequences of numbers can be studied conveniently '''
 
     images = np.concatenate([x, y], axis=1)
@@ -26,12 +26,13 @@ def plot_sequences(x, y, labels=None, output_path=None):
             plt.axis('off')
             counter += 1
         if labels is not None:
-            plt.title(labels[n_b],fontdict={'color':'white'})
+            plt.title(labels[n_b], fontdict={'color': 'white'})
 
     if output_path is not None:
         plt.savefig(output_path, dpi=600)
     else:
         plt.show()
+
 
 def get_lena():
     img = img = tf.io.read_file('data_util/resources/lena.jpg')
@@ -41,6 +42,58 @@ def get_lena():
     img = tf.image.convert_image_dtype(img, tf.float32)
     # resize the image to the desired size.
     return {"image": tf.image.resize(img, [300, 300])}
+
+def test_brain():
+    params = {
+        'dataset': 'ukb',
+        'preprocessing': [],
+        'dataset_dir': "/mnt/mpws2019cl1/brain_mri/tf_records",
+    }
+
+    f = functools.partial(
+        get_data,
+        split_name='train',
+        is_training=True,
+        num_epochs=1,
+        shuffle=False,
+        drop_remainder=True,
+        **params)
+
+    result = f({'batch_size': 1})
+    iterator = result.make_one_shot_iterator()
+
+    with tf.Session() as sess:
+        while True:
+            el = iterator.get_next()
+            batch = sess.run(el)
+            print(batch["image"].shape)
+
+
+def test_retina():
+    params = {
+        'dataset': 'retina',
+        'preprocessing': [],
+        'dataset_dir': "/mnt/mpws2019cl1/retinal_fundus/retina_tf_records/max_256/",
+    }
+
+    f = functools.partial(
+        get_data,
+        split_name='train',
+        is_training=True,
+        num_epochs=1,
+        shuffle=False,
+        drop_remainder=True,
+        **params)
+
+    result = f({'batch_size': 1})
+    iterator = result.make_one_shot_iterator()
+    el = iterator.get_next()
+    with tf.Session() as sess:
+        batch = sess.run(el)
+        print(batch["data"].shape)
+
+        plot_sequences(batch["data"], batch["data"], output_path=expanduser("~/test_retina.png"))
+
 
 def test_mnist_data_generator():
     flags.DEFINE_string('dataset', 'cpc_test', 'Which dataset to use, typically '
@@ -72,7 +125,7 @@ def test_mnist_data_generator():
 
 
 def show_batch(image_batch):
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(10, 10))
     dim = int(np.sqrt(len(image_batch)))
 
     if dim * dim < len(image_batch):
@@ -89,7 +142,7 @@ def show_batch(image_batch):
 def show_img(img):
     print(img.shape)
 
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(10, 10))
     plt.imshow(img)
     plt.show()
 
@@ -112,4 +165,5 @@ def test_preprocessing():
         print(patches["image"].shape)
         show_batch(patches["image"])
 
-test_preprocessing()
+
+test_brain()
