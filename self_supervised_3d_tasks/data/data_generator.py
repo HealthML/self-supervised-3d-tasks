71a7fcedd6a9229f542feba6adeaddc9a8df658b
tommysft <thgaertner.sft@t-online.de>
2020-01-10 15:45:30 +0100

    Create Keras Data Generator

--- self_supervised_3d_tasks/data/data_generator.py
+++ self_supervised_3d_tasks/data/data_generator.py
@@ -1,6 +1,7 @@
 import numpy as np
+from PIL import Image
 from tensorflow import keras
-from matplotlib import pyplot as plt
+import PIL
 import os
 
 
@@ -14,7 +15,7 @@ class DataGeneratorUnlabeled(keras.utils.Sequence):
                  dim=(32, 32, 32),
                  n_channels=1,
                  shuffle=True,
-                 image_format="jpeg"):
+                 preprocessing_functions=[]):
         '''
             :param data_path: path to directory with images
             :param file_list: list of files in directory for this data generator
@@ -22,7 +23,7 @@ class DataGeneratorUnlabeled(keras.utils.Sequence):
             :param dim: tuple of ints as dimension
             :param n_channels: number of channels
             :param shuffle: flag indicates shuffle after epoch
-            :param image_format: image format for loading (e.g. jpegs)
+            :param preprocessing_functions: list of callable preprocessing functions
         '''
         self.dim = dim
         self.batch_size = batch_size
@@ -31,7 +32,7 @@ class DataGeneratorUnlabeled(keras.utils.Sequence):
         self.n_channels = n_channels
         self.shuffle = shuffle
         self.path_to_data = data_path
-        self.image_format = image_format
+        self.preprocessing_functions = preprocessing_functions
         self.on_epoch_end()
 
     def __len__(self):
@@ -70,83 +71,18 @@ class DataGeneratorUnlabeled(keras.utils.Sequence):
         for i, ID in enumerate(list_IDs_temp):
             # Store sample
             path_to_image = "{}/{}".format(self.path_to_data, ID)
-            data_x[i,] = np.array(plt.imread(path_to_image, format=self.image_format))
+
+            im_frame = Image.open(path_to_image)
+            im_frame.resize(self.dim)
+            img = np.asarray(im_frame, dtype="float32")
+            img /= 255
+            for func in self.preprocessing_functions:
+                img = func(img)
+            data_x[i,] = img
         return data_x, data_y
 
 
-class DataGeneratorLabeled(keras.utils.Sequence):
-    'Generates data for Keras'
-
-    def __init__(self,
-                 data_path,
-                 file_list,
-                 batch_size=32,
-                 dim=(32, 32, 32),
-                 n_channels=1,
-                 shuffle=True,
-                 image_format="jpeg"):
-        '''
-            :param data_path: path to directory with images
-            :param file_list: list of files in directory for this data generator
-            :param batch_size: int batch size
-            :param dim: tuple of ints as dimension
-            :param n_channels: number of channels
-            :param shuffle: flag indicates shuffle after epoch
-            :param image_format: image format for loading (e.g. jpegs)
-        '''
-        self.dim = dim
-        self.batch_size = batch_size
-        self.list_IDs = file_list
-        self.indexes = np.arange(len(self.list_IDs))
-        self.n_channels = n_channels
-        self.shuffle = shuffle
-        self.path_to_data = data_path
-        self.image_format = image_format
-        self.on_epoch_end()
-
-    def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.list_IDs) / self.batch_size))
-
-    def __getitem__(self, index):
-        '''
-        Generate one batch of data
-        :param index: index of batch
-        :return: return list of file_names in this batch
-        '''
-        # Generate indexes of the batch
-        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
-
-        # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
-
-        # Generate data
-        X, Y = self.__data_generation(list_IDs_temp)
-
-        return X, Y
-
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.list_IDs))
-        if self.shuffle:
-            np.random.shuffle(self.indexes)
-
-    def __data_generation(self, list_IDs_temp):
-        'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
-        # Initialization
-        data_x = np.empty((self.batch_size, *self.dim, self.n_channels))
-        # TODO: Adjust labels
-        data_y = [0 * self.batch_size]
-        # Generate data
-        for i, ID in enumerate(list_IDs_temp):
-            # Store sample
-            path_to_image = "{}/{}".format(self.path_to_data, ID)
-            data_x[i,] = np.array(plt.imread(path_to_image, format=self.image_format))
-        return data_x, data_y
-
-
-
-def get_data_generators(data_path, train_split=.6, labeled = False, val_split=None, shuffle_files=True, data_generator_args={}):
+def get_data_generators(data_path, train_split=.6, val_split=None, shuffle_files=True, data_generator_args={}):
     '''
     This function generates the data generator for training, testing and optional validation.
     :param data_path: path to files
@@ -176,15 +112,10 @@ def get_data_generators(data_path, train_split=.6, labeled = False, val_split=No
         val = files[train_split:train_split + val_split]
         test = files[train_split + val_split:]
 
-        # Create data generators
-        if labeled:
-            train_data_generator = DataGeneratorLabeled(data_path, train, **data_generator_args)
-            test_data_generator = DataGeneratorLabeled(data_path, test, **data_generator_args)
-            val_data_generator = DataGeneratorLabeled(data_path, val, **data_generator_args)
-        else:
-            train_data_generator = DataGeneratorUnlabeled(data_path, train, **data_generator_args)
-            test_data_generator = DataGeneratorUnlabeled(data_path, test, **data_generator_args)
-            val_data_generator = DataGeneratorUnlabeled(data_path, val, **data_generator_args)
+
+        train_data_generator = DataGeneratorUnlabeled(data_path, train, **data_generator_args)
+        test_data_generator = DataGeneratorUnlabeled(data_path, test, **data_generator_args)
+        val_data_generator = DataGeneratorUnlabeled(data_path, val, **data_generator_args)
         # Return generators
         return train_data_generator, test_data_generator, val_data_generator
     else:
@@ -198,12 +129,8 @@ def get_data_generators(data_path, train_split=.6, labeled = False, val_split=No
         test = files[train_split:]
 
         # Create data generators
-        if labeled:
-            train_data_generator = DataGeneratorLabeled(data_path, train, **data_generator_args)
-            test_data_generator = DataGeneratorLabeled(data_path, test, **data_generator_args)
-        else:
-            train_data_generator = DataGeneratorUnlabeled(data_path, train, **data_generator_args)
-            test_data_generator = DataGeneratorUnlabeled(data_path, test, **data_generator_args)
+        train_data_generator = DataGeneratorUnlabeled(data_path, train, **data_generator_args)
+        test_data_generator = DataGeneratorUnlabeled(data_path, test, **data_generator_args)
 
         # Return generators
         return train_data_generator, test_data_generator
