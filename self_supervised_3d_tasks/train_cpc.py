53d11f47993e26c28b05531f162718a697f8679e
tommysft <thgaertner.sft@t-online.de>
2020-01-13 09:24:08 +0100

    Bugfix

--- self_supervised_3d_tasks/train_cpc.py
+++ self_supervised_3d_tasks/train_cpc.py
@@ -24,7 +24,7 @@ from self_supervised_3d_tasks.ifttt_notify_me import shim_outputs, Tee
 from self_supervised_3d_tasks.free_gpu_check import aquire_free_gpus
 from contextlib import redirect_stdout, redirect_stderr
 
-aquire_free_gpus(1)
+aquire_free_gpus(2)
 c_stdout, c_stderr = shim_outputs()  # I redirect stdout / stderr to later inform us about errors
 
 
@@ -103,13 +103,14 @@ def train_model(epochs, code_size, lr=1e-4, terms=4, predict_terms=4, image_size
     f_val = lambda x, y: preprocess_grid(preprocess(x, crop_size, split_per_side, is_training=False))
 
     train_data, validation_data = get_data_generators(data_dir, train_split=0.7,
-                                                      train_data_generator_args={"batch_size": batch_size, "dim": (192,192),
-                                                                           "n_channels": n_channels,
-                                                                           "func": f_train},
-                                                      test_data_generator_args={"batch_size": batch_size,
+                                                      train_data_generator_args={"batch_size": batch_size,
                                                                                  "dim": (192, 192),
                                                                                  "n_channels": n_channels,
-                                                                                 "func": f_val}
+                                                                                 "pre_proc_func": f_train},
+                                                      test_data_generator_args={"batch_size": batch_size,
+                                                                                "dim": (192, 192),
+                                                                                "n_channels": n_channels,
+                                                                                "pre_proc_func": f_val}
                                                       )
 
     # Prepare data
@@ -127,9 +128,10 @@ def train_model(epochs, code_size, lr=1e-4, terms=4, predict_terms=4, image_size
                                               embeddings_layer_names=None, embeddings_metadata=None,
                                               embeddings_data=None, update_freq='batch')
 
-    mc_callback = keras.callbacks.ModelCheckpoint(working_dir, monitor='val_loss', verbose=0,
-                                                  save_best_only=False,
-                                                  save_weights_only=False, mode='auto', period=1)
+    mc_callback = keras.callbacks.ModelCheckpoint(
+        working_dir + "/weights-improvement-{epoch:02d}-{val_accuracy:.2f}.hdf5", monitor='val_loss', verbose=0,
+        save_best_only=False,
+        save_weights_only=False, mode='auto', period=1)
 
     callbacks = [keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=1 / 3, patience=2, min_lr=1e-4),
                  tb_callback, mc_callback]
