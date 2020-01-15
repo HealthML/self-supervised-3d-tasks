c75bc24479a8bcfad45d6448cb51d7ccfab6b517
Winfried Loetzsch <winfried.loetzsch@student.hpi.de>
2020-01-15 14:19:56 +0100

    loading model

--- self_supervised_3d_tasks/run_cpc_tests.py
+++ self_supervised_3d_tasks/run_cpc_tests.py
@@ -1,4 +1,5 @@
 from self_supervised_3d_tasks.algorithms.contrastive_predictive_coding import network_cpc
+import keras
 
 epochs = 10,
 code_size = 128,
@@ -8,8 +9,17 @@ predict_terms = 3,
 image_size = 46,
 batch_size = 8
 n_channels = 3
+crop_size = 186
+split_per_side = 7
+img_shape = (image_size, image_size, n_channels)
 
-model, enc_model = network_cpc(image_shape=(image_size, image_size, n_channels), terms=terms,
+model, enc_model = network_cpc(image_shape=img_shape, terms=terms,
                                predict_terms=predict_terms, code_size=code_size, learning_rate=lr)
+
+# we need so many models
+layer_in = [keras.Input(img_shape)] * (split_per_side * split_per_side)
+layer_out = [enc_model(x) for x in layer_in]
+
+
 
 model.load_weights('my_model_weights.h5')
