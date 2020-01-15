3f98a28e03330df69ec267ed119af1cac345efe1
Julius Severin <julius.severin@student.hpi.de>
2020-01-15 14:31:10 +0100

    fix free gpu method to ignore GPU 0

--- self_supervised_3d_tasks/free_gpu_check.py
+++ self_supervised_3d_tasks/free_gpu_check.py
@@ -15,21 +15,20 @@ def aquire_free_gpus(amount=1):
     gpu_df["memory.free"] = gpu_df["memory.free"].map(lambda x: int(x.rstrip(" [MiB]")))
     gpu_df["memory.used"] = gpu_df["memory.used"].map(lambda x: int(x.rstrip(" [MiB]")))
     gpu_df = gpu_df.sort_values(by=["memory.free"], ascending=False)
+    gpu_df = gpu_df.sort_values(by=["memory.used"], ascending=True)
+    # TODO: Fix this as soon as GPU is fixed
+    gpu_df.drop(0, inplace=True)
     print(gpu_df)
     output = []
 
     if len(gpu_df) < amount:
         raise ValueError("The requested amount of GPUs is not existing.")
     for i in range(amount):
-        max_gpu = gpu_df["memory.free"].idxmax()
+        max_gpu = gpu_df["memory.used"].idxmin()
         if gpu_df.loc[max_gpu]["memory.used"] != 0:
             raise ValueError(
                 "The requested amount of GPUs are not available currently."
             )
-
-        # TODO: Fix this
-        max_gpu = 4
-
         output.append(max_gpu)
         gpu_df.drop(max_gpu, inplace=True)
     os.environ["CUDA_VISIBLE_DEVICES"] = str(output).strip("[").strip("]")
