0405d2f7fa6d2f51dac65438b7b4745ecdbf8d2a
Winfried Loetzsch <winfried.loetzsch@student.hpi.de>
2020-01-20 14:53:58 +0100

    started to implement jigsaw

---
+++ self_supervised_3d_tasks/keras_algorithms/jigsaw.py
@@ -0,0 +1,25 @@
+from keras.layers import TimeDistributed
+
+from self_supervised_3d_tasks.algorithms import patch_utils
+
+from self_supervised_3d_tasks.keras_models.res_net_2d import get_res_net_2d
+
+dim=(192, 192)
+n_channels=3
+lr=1e-3
+embed_dim=1000
+architecture="ResNet50"
+
+def get_training_model():
+    model = get_res_net_2d(input_shape=[*dim, n_channels], classes=embed_dim, architecture=architecture,
+                           learning_rate=lr)
+    model = TimeDistributed(model)
+    return model
+
+def get_training_generators(batch_size, dataset_name):
+    perms, num_classes = patch_utils.load_permutations()
+
+def get_finetuning_generators(batch_size, dataset_name, training_proportion):
+
+
+def get_finetuning_model(load_weights, freeze_weights):
