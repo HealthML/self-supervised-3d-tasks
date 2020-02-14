eb2467f4ec05c834db95c7ecb43c39f53b356ef0
hv10 <noeldanz@gmail.com>
2020-02-14 15:16:34 +0100

    update hyperopt capabilities to current arch update available model architectures

--- self_supervised_3d_tasks/keras_algorithms/custom_utils.py
+++ self_supervised_3d_tasks/keras_algorithms/custom_utils.py
@@ -8,8 +8,8 @@ from pathlib import Path
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Model, Input
-from tensorflow.keras.applications import InceptionV3
-from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import InceptionV3, InceptionResNetV2, ResNet152
+from tensorflow.keras.applications import ResNet50, ResNet50V2, ResNet101, ResNet101V2
 from tensorflow.keras.layers import Dense, Flatten
 
 from self_supervised_3d_tasks.free_gpu_check import aquire_free_gpus
@@ -32,17 +32,24 @@ def init(f, name="training", NGPUS=1):
     if "log_level" in args:
         tf.get_logger().setLevel(args["log_level"])
     else:
-        tf.get_logger().setLevel('ERROR')
+        tf.get_logger().setLevel("ERROR")
 
     print("###########################################")
     print("{} {} with parameters: ".format(name, args))
     print("###########################################")
 
     aquire_free_gpus(amount=NGPUS, **args)
-    c_stdout, c_stderr = shim_outputs()  # I redirect stdout / stderr to later inform us about errors
+    (
+        c_stdout,
+        c_stderr,
+    ) = shim_outputs()  # I redirect stdout / stderr to later inform us about errors
 
-    with redirect_stdout(Tee(c_stdout, sys.stdout)):  # needed to actually capture stdout
-        with redirect_stderr(Tee(c_stderr, sys.stderr)):  # needed to actually capture stderr
+    with redirect_stdout(
+            Tee(c_stdout, sys.stdout)
+    ):  # needed to actually capture stdout
+        with redirect_stderr(
+                Tee(c_stderr, sys.stderr)
+        ):  # needed to actually capture stderr
             f(**args)
 
 
@@ -57,8 +64,13 @@ def get_prediction_model(name, in_shape):
     return model
 
 
-def apply_prediction_model(input_shape, n_prediction_layers=2, dim_prediction_layers=1024, prediction_architecture=None,
-                           **kwargs):
+def apply_prediction_model(
+        input_shape,
+        n_prediction_layers=2,
+        dim_prediction_layers=1024,
+        prediction_architecture=None,
+        **kwargs
+):
     if prediction_architecture is not None:
         model = get_prediction_model(prediction_architecture, input_shape)
     else:
@@ -76,9 +88,33 @@ def apply_prediction_model(input_shape, n_prediction_layers=2, dim_prediction_la
 
 def get_encoder_model(name, in_shape, pooling):
     if name == "InceptionV3":
-        model = InceptionV3(include_top=False, input_shape=in_shape, weights=None, pooling=pooling)
-    elif name == "Resnet50":
-        model = ResNet50(include_top=False, input_shape=in_shape, weights=None, pooling=pooling)
+        model = InceptionV3(
+            include_top=False, input_shape=in_shape, weights=None, pooling=pooling
+        )
+    elif name == "ResNet50":
+        model = ResNet50(
+            include_top=False, input_shape=in_shape, weights=None, pooling=pooling
+        )
+    elif name == "ResNet50V2":
+        model = ResNet50V2(
+            include_top=False, input_shape=in_shape, weights=None, pooling=pooling
+        )
+    elif name == "ResNet101":
+        model = ResNet101(
+            include_top=False, input_shape=in_shape, weights=None, pooling=pooling
+        )
+    elif name == "ResNet101V2":
+        model = ResNet101V2(
+            include_top=False, input_shape=in_shape, weights=None, pooling=pooling
+        )
+    elif name == "ResNet152":
+        model = ResNet152(
+            include_top=False, input_shape=in_shape, weights=None, pooling=pooling
+        )
+    elif name == "InceptionResNetV2":
+        model = InceptionResNetV2(
+            include_top=False, input_shape=in_shape, weights=None, pooling=pooling
+        )
     else:
         raise ValueError("model " + name + " not found")
 
@@ -89,20 +125,36 @@ def get_encoder_model_3d(name, in_shape):
     raise ValueError("model " + name + " not found")
 
 
-def apply_encoder_model_3d(input_shape, code_size, num_layers=4, pooling="max", encoder_architecture=None, **kwargs):
+def apply_encoder_model_3d(
+        input_shape,
+        code_size,
+        num_layers=4,
+        pooling="max",
+        encoder_architecture=None,
+        **kwargs
+):
     if encoder_architecture is not None:
         model = get_encoder_model_3d(encoder_architecture, input_shape)
     else:
-        model, _ = downconv_model_3d(input_shape, num_layers=num_layers, pooling=pooling)
+        model, _ = downconv_model_3d(
+            input_shape, num_layers=num_layers, pooling=pooling
+        )
 
     x = Flatten()(model.outputs[0])
     x = Dense(code_size)(x)
 
-    enc_model = Model(model.inputs[0], x, name='encoder')
+    enc_model = Model(model.inputs[0], x, name="encoder")
     return enc_model
 
 
-def apply_encoder_model(input_shape, code_size, num_layers=4, pooling="max", encoder_architecture=None, **kwargs):
+def apply_encoder_model(
+        input_shape,
+        code_size,
+        num_layers=4,
+        pooling="max",
+        encoder_architecture=None,
+        **kwargs
+):
     if encoder_architecture is not None:
         model = get_encoder_model(encoder_architecture, input_shape, pooling)
     else:
@@ -111,12 +163,15 @@ def apply_encoder_model(input_shape, code_size, num_layers=4, pooling="max", enc
     x = Flatten()(model.outputs[0])
     x = Dense(code_size)(x)
 
-    enc_model = Model(model.inputs[0], x, name='encoder')
+    enc_model = Model(model.inputs[0], x, name="encoder")
     return enc_model
 
 
-def load_permutations_3d(permutation_path=str(Path(__file__).parent.parent / "permutations" /
-                                              "permutations3d_100_max.bin")):
+def load_permutations_3d(
+        permutation_path=str(
+            Path(__file__).parent.parent / "permutations" / "permutations3d_100_max.bin"
+        ),
+):
     with open(permutation_path, "rb") as f:
         int32_size = 4
         s = f.read(int32_size * 2)
@@ -131,8 +186,11 @@ def load_permutations_3d(permutation_path=str(Path(__file__).parent.parent / "pe
     return perms, num_perms
 
 
-def load_permutations(permutation_path=str(Path(__file__).parent.parent / "permutations" /
-                                           "permutations_100_max.bin")):
+def load_permutations(
+        permutation_path=str(
+            Path(__file__).parent.parent / "permutations" / "permutations_100_max.bin"
+        ),
+):
     """Loads a set of pre-defined permutations."""
     with open(permutation_path, "rb") as f:
         int32_size = 4
@@ -156,7 +214,7 @@ def get_writing_path(working_dir, root_config_file):
     i = 1
     while Path(working_dir).exists():
         if i > 1:
-            working_dir = working_dir[:-len(str(i - 1))]
+            working_dir = working_dir[: -len(str(i - 1))]
         else:
             working_dir += "_"
 
--- self_supervised_3d_tasks/keras_algorithms/keras_hyperopt_algo.py
+++ self_supervised_3d_tasks/keras_algorithms/keras_hyperopt_algo.py
@@ -1,21 +1,56 @@
 from functools import partial
 
-from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
-from sklearn.metrics import roc_auc_score, cohen_kappa_score
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials, STATUS_FAIL
 import sys
 
-from self_supervised_3d_tasks.keras_algorithms.custom_utils import init
-from self_supervised_3d_tasks.keras_algorithms.keras_train_algo import train_model
+from self_supervised_3d_tasks.free_gpu_check import aquire_free_gpus
+
+from self_supervised_3d_tasks.keras_algorithms.keras_test_algo import (
+    get_dataset_test,
+    run_single_test,
+)
+from self_supervised_3d_tasks.keras_algorithms.keras_train_algo import (
+    keras_algorithm_list,
+)
+
+aquire_free_gpus(amount=1)
+
 
 def f_nn(params):
-    part_f = partial(train_model, epochs=params["epochs"], batch_size=params["batch_size"],
-                     model_params=params["model_params"])
-    model, validation_data = init(part_f, name="hyperopt", algorithm=params["algorithm"], dataset=params["dataset"], NGPUS=params["NGPUS"])
-    y_pred = model.predict(validation_data[0])
-    kappa = cohen_kappa_score(validation_data[1], y_pred)
-    print('Kappa:', kappa)
-    sys.stdout.flush()
-    return {'loss': (1 - abs(kappa)), 'status': STATUS_OK}
+    try:
+        print(params)
+        sys.stdout.flush()
+        algorithm_def = keras_algorithm_list[params["algorithm"]].create_instance(
+            **params["model_params"]
+        )
+        f_train, f_val = algorithm_def.get_finetuning_preprocessing()
+        x_test, y_test = get_dataset_test(
+            params["dataset"], params["batch_size"], f_train, f_val
+        )
+        params["model_params"]["num_layers"] = int(params["model_params"]["num_layers"])
+        kappa = run_single_test(
+            algorithm_def=algorithm_def,
+            dataset_name=params["dataset"],
+            train_split=1,
+            load_weights=False,
+            freeze_weights=False,
+            x_test=x_test,
+            y_test=y_test,
+            lr=1 / params["lr"],
+            batch_size=int(params["batch_size"]),
+            epochs=int(params["epochs"]),
+            epochs_warmup=0,
+            model_checkpoint=None,
+            kwargs=params["model_params"],
+        )
+        print("Kappa:", kappa)
+        sys.stdout.flush()
+        return {"loss": ((-1 * kappa) / 2) + 0.5, "status": STATUS_OK}
+    except Exception as e:
+        print("Configuration Failed")
+        print(e)
+        sys.stdout.flush()
+        return {"loss": float("inf"), "status": STATUS_FAIL}
 
 
 def hyperopt_model(space):
@@ -25,13 +60,29 @@ def hyperopt_model(space):
     with open("~/hyperopt.txt") as f:
         f.write(str(best))
 
-if __name__=="__main__":
-    space={
-        "algorithm":"jigsaw",
-        "epochs": 250,
-        "batch_size": 16,
-        "model_params":{
 
-        }
+if __name__ == "__main__":
+    space = {
+        "algorithm": "rotation",
+        "dataset": "kaggle_retina",
+        "epochs": 5,
+        "batch_size": hp.uniform("batch_size", 2, 64),
+        "lr": hp.lognormal("learning_rate", 7.0, 2.5),
+        "model_params": {
+            "embed_dim": hp.uniform("embed_dim", 128, 20000),
+            "encoder_architecture": hp.choice("architecture",
+                                              [
+                                                  None,
+                                                  "ResNet50",
+                                                  "InceptionV3",
+                                                  "ResNet50V2",
+                                                  "ResNet101",
+                                                  "ResNet101V2",
+                                                  "InceptionResNetV2",
+                                              ]
+                                              ),
+            "pooling": hp.choice("pooling", ["max", "avg"]),
+            "num_layers": hp.uniform("num_layers", 2, 6),
+        },
     }
     hyperopt_model(space)
