c3c77fec20dd861aaee1392b25f9735e1c8136bc
tommysft <thgaertner.sft@t-online.de>
2020-02-19 14:26:26 +0100

    bugfix exemplar

--- self_supervised_3d_tasks/keras_algorithms/exemplar.py
+++ self_supervised_3d_tasks/keras_algorithms/exemplar.py
@@ -1,19 +1,12 @@
 import numpy as np
 
 from self_supervised_3d_tasks.custom_preprocessing.preprocessing_exemplar import preprocessing_exemplar
-
-from tensorflow.keras.optimizers import Adam
-
 from self_supervised_3d_tasks.keras_algorithms.custom_utils import apply_encoder_model_3d, apply_encoder_model
 
-np.random.seed(0)
-
-from tensorflow.keras.layers import Input, Concatenate, Lambda, Flatten, Dense
-from tensorflow.keras.models import Model
-
-from tensorflow.keras import Sequential
-
-import tensorflow.keras.backend as K
+from tensorflow.keras.layers import Concatenate, Lambda, Flatten, Input
+import tensorflow.python.keras.backend as K
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import load_model, Model, Sequential
 
 
 class ExemplarBuilder:
@@ -25,7 +18,7 @@ class ExemplarBuilder:
             dim_3d=False,
             alpha_triplet=0.2,
             embedding_size=10,
-            lr = 0.0006,
+            lr=0.0006,
             model_checkpoint=None,
             **kwargs
     ):
@@ -50,8 +43,8 @@ class ExemplarBuilder:
         """
         embeddings = K.reshape(y_pred, (-1, 3, self.embedding_size))
 
-        positive_distance = K.mean(K.square(embeddings[:,0] - embeddings[:,1]),axis=-1)
-        negative_distance = K.mean(K.square(embeddings[:,0] - embeddings[:,2]),axis=-1)
+        positive_distance = K.mean(K.square(embeddings[:, 0] - embeddings[:, 1]), axis=-1)
+        negative_distance = K.mean(K.square(embeddings[:, 0] - embeddings[:, 2]), axis=-1)
         return K.mean(K.maximum(0.0, positive_distance - negative_distance + _alpha))
 
     def apply_model(self, input_shape, **kwargs):
@@ -63,18 +56,22 @@ class ExemplarBuilder:
         :return: return the network (encoder) and the compiled model with concated output
         """
         # defines encoder
-        network = apply_encoder_model_3d((*self.dim, self.number_channels), self.embedding_size, **self.kwargs) if self.dim_3d else apply_encoder_model((*self.dim, self.number_channels), self.embedding_size, **self.kwargs)
+        if self.dim_3d:
+            network = apply_encoder_model_3d((*self.dim, self.number_channels), self.embedding_size, **self.kwargs)
+        else:
+            network = apply_encoder_model((*self.dim, self.number_channels), self.embedding_size, **self.kwargs)
+
         # Define the tensors for the three input images
 
-        input = Input((3, *input_shape), name="anchor_input")
-        anchor_input = Lambda(lambda x: x[0, :])(input)
-        positive_input = Lambda(lambda x: x[1, :])(input)
-        negative_input = Lambda(lambda x: x[2, :])(input)
+        input = Input((3, *input_shape), name="Input")
+        anchor_input = Lambda(lambda x: x[:, 0, :], name="anchor_input")(input)
+        positive_input = Lambda(lambda x: x[:, 1, :], name="positive_input")(input)
+        negative_input = Lambda(lambda x: x[:, 2, :], name="negative_input")(input)
 
         # Generate the encodings (feature vectors) for the three images
-        encoded_a = network(anchor_input)
-        encoded_p = network(positive_input)
-        encoded_n = network(negative_input)
+        encoded_a = Sequential(network, name="encoder_a")(anchor_input)
+        encoded_p = Sequential(network, name="encoder_p")(positive_input)
+        encoded_n = Sequential(network, name="encoder_n")(negative_input)
 
         # Concat the outputs together
         output = Concatenate()([encoded_a, encoded_p, encoded_n])
@@ -84,15 +81,13 @@ class ExemplarBuilder:
 
         # Connect the inputs with the outputs
         model = Model(inputs=input, outputs=output)
-        print(model.summary())
+        print(network.summary())
         # compile the model
         model.compile(loss=self.triplet_loss, optimizer=optimizer)
         return network, model
 
-
     def get_training_model(self):
         return self.apply_model((*self.dim, self.number_channels))[1]
-
 
     def get_training_preprocessing(self):
         def f_train(x, y):
@@ -103,7 +98,6 @@ class ExemplarBuilder:
 
         return f_train, f_val
 
-
     def get_finetuning_preprocessing(self):
         def f_train(x, y):
             return preprocessing_exemplar(x, y, self.dim_3d)
@@ -112,7 +106,6 @@ class ExemplarBuilder:
             return preprocessing_exemplar(x, y, self.dim_3d)
 
         return f_train, f_val
-
 
     def get_finetuning_layers(self, load_weights, freeze_weights):
         enc_model, model_full = self.apply_model((*self.dim, self.number_channels))
@@ -129,6 +122,7 @@ class ExemplarBuilder:
 
         x = Flatten()(layer_out)
         return layer_in, x, [enc_model, model_full]
+
 
 def create_instance(*params, **kwargs):
     return ExemplarBuilder(*params, **kwargs)
