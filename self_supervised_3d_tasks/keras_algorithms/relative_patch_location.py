de693aa5bf29a1effaaeeef82bcbd721ef29dda9
Julius Severin <julius.severin@student.hpi.de>
2020-02-21 18:39:39 +0100

    two inputs for rpl

--- self_supervised_3d_tasks/keras_algorithms/relative_patch_location.py
+++ self_supervised_3d_tasks/keras_algorithms/relative_patch_location.py
@@ -31,41 +31,50 @@ class RelativePatchLocationBuilder:
         self.data_dim = data_dim
         self.embed_dim = embed_dim
         self.n_channels = n_channels
-        self.patches_per_side = patches_per_side
         self.patch_jitter = patch_jitter
         self.lr = lr
-        self.image_size = int(data_dim / patches_per_side) - patch_jitter
-        self.img_shape = (self.image_size, self.image_size, n_channels)
-        self.img_shape_3d = (self.image_size, self.image_size, self.image_size, n_channels)
         self.train3D = train3D
         self.kwargs = kwargs
         self.top_architecture = top_architecture
 
+        self.patches_per_side = patches_per_side
+        self.image_size = int(data_dim / patches_per_side) - patch_jitter
+        self.image_shape = (self.image_size, self.image_size, n_channels)
+        self.patch_count = patches_per_side**2
+        if self.train3D:
+            self.image_shape = (self.image_size, ) + self.image_shape
+            self.patch_count = self.patches_per_side**3
+
+        self.images_shape = (2, ) + self.image_shape
+        self.class_count = self.patch_count - 1
+
     def apply_model(self):
         if self.train3D:
             enc_model = apply_encoder_model_3d(
-                self.img_shape_3d, self.embed_dim, **self.kwargs
+                self.image_shape, self.embed_dim, **self.kwargs
             )
+            enc_model = enc_model[0]
             a = apply_prediction_model(
                 self.embed_dim,
                 prediction_architecture=self.top_architecture,
                 include_top=False,
             )
-            x = Dense(27, activation="softmax")
         else:
             enc_model = apply_encoder_model(
-                self.img_shape, self.embed_dim, **self.kwargs
+                self.image_shape, self.embed_dim, **self.kwargs
             )
             a = apply_prediction_model(
                 self.embed_dim,
                 prediction_architecture=self.top_architecture,
                 include_top=False,
             )
-            # TODO Changed 4 => 9 (classes)
-            x = Dense(9, activation="softmax")
 
-        model = Sequential([enc_model, a, x])
-        enc_model.summary()
+        x_input = Input(self.images_shape)
+        enc_out = TimeDistributed(enc_model)(x_input)
+        enc_out = Flatten()(enc_out)
+        out = Dense(self.class_count, activation="softmax")(enc_out)
+
+        model = Model(x_input, out)
         model.summary()
 
         return model, enc_model
@@ -80,16 +89,6 @@ class RelativePatchLocationBuilder:
         )
 
         return model
-    #
-    # def get_training_model(self):
-    #     model = get_res_net_2d(
-    #         input_shape=self.img_shape,
-    #         classes=self.patches_per_side ** 2,
-    #         architecture="ResNet50",
-    #         learning_rate=self.lr,
-    #     )
-    #
-    #     return model
 
     def get_training_preprocessing(self):
         def f(x, y):  # not using y here, as it gets generated
@@ -139,7 +138,7 @@ class RelativePatchLocationBuilder:
         if model_checkpoint is not None:
             model.load_weights(model_checkpoint)
 
-        layer_in = Input((self.patches_per_side * self.patches_per_side,) + self.img_shape)
+        layer_in = Input((self.patches_per_side * self.patches_per_side,) + self.image_shape)
         layer_out = TimeDistributed(model)(layer_in)
 
         x = Flatten()(layer_out)
